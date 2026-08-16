fe4f5cf428679f2a5c753c39751598763dcc43b1
joaofelipemx <joao.teodoro@escolar.ifrn.edu.br>
2026-08-16 15:06:26 -0300

    feat: criação dos componentes e inicio do App.jsx

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,9 @@
+function Cabecalho() {
+  return (
+    <div>
+      <h1>Placar Digital Esportivo</h1>
+    </div>
+  )
+}
+
+export default Cabecalho
---
+++ src/components/Time.jsx
@@ -0,0 +1,20 @@
+function Time( { nome, pontos, setPontos, faltas, setFaltas, incrementar, decrementar, zerar }) {
+    return(
+        <div className={vencendo ? "vencendo" : ""}>
+            <h2>{nome}</h2>
+            <p>Pontos: {pontos}</p>
+            <p>Faltas: {faltas}</p>
+
+            <p>Pontos</p>
+            <button onClick={() => incrementar(pontos, setPontos, 1)}>+1</button>
+            <button onClick={() => incrementar(pontos, setPontos, 2)}>+2</button>
+            <button onClick={() => incrementar(pontos, setPontos, 3)}>+3</button>
+
+            <p>Faltas</p>
+            <button onClick={() => incrementar(faltas, setFaltas, 1)}>+1</button>
+            <button onClick={() => decrementar(faltas, setFaltas, 1)}>-1</button>
+
+
+        </div>
+    )
+}
